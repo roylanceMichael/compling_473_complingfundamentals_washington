e9dd65516430d2b40fc18b83150d57a1a99538ba
Mike Roylance <roylance.michael@gmail.com>
2013-08-08 17:28:52 -0600

    changed the bash script

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
-ruby ./source/main.rb $1
+#!/bin/sh
+if [ -n "$1" ] # note the quoted test strings
+then
+  ruby ./source/main.rb /corpora/LDC/LDC99T24/RAW/parsed/prd/wsj/14
+else  
+	ruby ./source/main.rb $1
+fi
