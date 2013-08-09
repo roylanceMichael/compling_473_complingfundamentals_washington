7808d4b58cc7b97bf4dc7433662145d6ef5068bf
Mike Roylance <roylance.michael@gmail.com>
2013-08-08 21:27:58 -0600

    adding in the log and error files

--- condor.cmd
+++ condor.cmd
@@ -1,5 +1,6 @@
-Executable = run.sh
+executable = run.sh
 output = output.txt
-log = log.txt
+log = tagCounter.log
+error = tagCounter.error
 arguments = "./source/main.rb /corpora/LDC/LDC99T42/RAW/parsed/prd/wsj/14"
 queue
