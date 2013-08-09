fd5a2ffb08eb16a52e429e71317d33a8e7e9c78f
Mike Roylance <roylance.michael@gmail.com>
2013-08-08 20:00:00 -0600

    updated the run.sh file

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-if [ -n "$1" ] # note the quoted test strings
+if [ -n $1 ]
 then
-  ruby ./source/main.rb /corpora/LDC/LDC99T24/RAW/parsed/prd/wsj/14
+  ruby ./source/main.rb /corpora/LDC/LDC99T42/RAW/parsed/prd/wsj/14
 else  
 	ruby ./source/main.rb $1
 fi
