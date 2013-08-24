cfc02a1602fa6dadd7f9a7b5aa8a16066e454410
Mike Roylance <roylance.michael@gmail.com>
2013-08-23 21:08:50 -0600

    uploaded the actual ascii values

--- project3/source/fsm.cs
+++ project3/source/fsm.cs
@@ -19,13 +19,15 @@ namespace Ling473_Proj3
 {
 	public class Fsm
 	{
-		private static HashSet<Char> V1 = new HashSet<Char>("เแโใไ");
-		private static HashSet<Char> C1 = new HashSet<Char>("กขฃคฅฆงจฉชซฌญฎฏฐฑฒณดตถทธนบปผฝพฟภมยรฤลฦวศษสหฬอฮ");
-		private static HashSet<Char> C2 = new HashSet<Char>("รลวนม");
-		private static HashSet<Char> V2 = new HashSet<Char>("ิีึืุูั็");
+		private static HashSet<Char> V1 = new HashSet<Char>("\u0E40\u0E41\u0E42\u0E43\u0E44");
+		private static HashSet<Char> C1 = new HashSet<Char>("\u0E01\u0E02\u0E03\u0E04\u0E05\u0E06\u0E07\u0E08\u0E09\u0E0A\u0E0B\u0E0C\u0E0D\u0E0E\u0E0F"
+                + "\u0E10\u0E11\u0E12\u0E13\u0E14\u0E15\u0E16\u0E17\u0E18\u0E19\u0E1A\u0E1B\u0E1C\u0E1D\u0E1E\u0E1F"
+                + "\u0E20\u0E21\u0E22\u0E23\u0E24\u0E25\u0E26\u0E27\u0E28\u0E29\u0E2A\u0E2B\u0E2C\u0E2D\u0E2E");
+		private static HashSet<Char> C2 = new HashSet<Char>("\u0E23\u0E25\u0E27\u0E19\u0E21");
+		private static HashSet<Char> V2 = new HashSet<Char>("\u0E34\u0E35\u0E36\u0E37\u0E38\u0E39\u0E31\u0E47");
 		private static HashSet<Char> T = new HashSet<Char> { '\u0E48', '\u0E49', '\u0E4A', '\u0E4B' };
-		private static HashSet<Char> V3 = new HashSet<Char>("าอยว");
-		private static HashSet<Char> C3 = new HashSet<Char>("งนมดบกยว");
+		private static HashSet<Char> V3 = new HashSet<Char>("\u0E32\u0E2D\u0E22\u0E27");
+		private static HashSet<Char> C3 = new HashSet<Char>("\u0E07\u0E19\u0E21\u0E14\u0E1A\u0E01\u0E22\u0E27");
 
 		private Dictionary<int, Func<char, int>> Transitions;
 
