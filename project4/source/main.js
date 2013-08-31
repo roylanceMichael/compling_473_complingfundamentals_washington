49a2246c873feada64e13231c2520cc28829c1cd
Mike Roylance <roylance.michael@gmail.com>
2013-08-30 20:19:57 -0600

    working, although I fear node is too slow

--- project4/source/main.js
+++ project4/source/main.js
@@ -8,14 +8,23 @@ if(process.argv == undefined || process.argv.length < 3) {
 	return;
 }
 
+var print = function(val) {
+	if(debug) {
+		console.log(val);
+	}
+}
+
+// local variables
+var debug = false;
 var folderLocation = process.argv[2];
 var filesToProcess = [];
-var sevenTargetsFile = "../content/seven-targets.txt";
-var sevenTargets = fs.createReadStream(sevenTargetsFile);
+var targetsFile = "../content/seven-targets.txt";
+var trieStream = fs.createReadStream(targetsFile);
 var dnaFile = /\.dna$/;
-
 var rootTrie = {};
 var currentNode = rootTrie;
+// this will tell us how far in advance we need to search
+var maximumTrieSize = 0;
 
 // get all the files to process
 fs.readdir(folderLocation, function(err, files) {
@@ -27,8 +36,12 @@ fs.readdir(folderLocation, function(err, files) {
 });
 
 // load up the trie
-sevenTargets.on('data', function(buffer) {
+trieStream.on('data', function(buffer) {
 	var bufferStr = buffer.toString();
+	
+	// get the maximum size allowable
+	maximumTrieSize = maximumTrieSize + bufferStr.length;
+
 	var buildResult = buildTrie(bufferStr, currentNode, rootTrie);
 
 	// update values
@@ -36,66 +49,64 @@ sevenTargets.on('data', function(buffer) {
 	currentNode = buildResult.currentNode;
 });
 
-sevenTargets.on('end', function() {
+// after the trie has been loaded, process each file
+trieStream.on('end', function() {
 	// we have our main trie now
-	console.log("trie built, now searching " + filesToProcess.length + " file");
+	print("trie built, now searching " + filesToProcess.length + " file(s) with a maxium trie size of " + maximumTrieSize);
 
 	for(var i = 0; i < filesToProcess.length; i++) {
-
-		var fileStream = fs.createReadStream(filesToProcess[i]);
-		var offset = 0;
-
-		// will contain structure like this { "state":"mada", "currentNode": currentNode, "foundTrieSoFar": foundTrieSoFar }
-		var inProgressSearches = [];
-		var successfulSearches = [];
-
-		fileStream.on('data', function(buffer) {
-
-			var bufferStr = buffer.toString();
-
-			// we need to process each inProgress
-			var tmpArray = [];
-			for(var j = 0; j < inProgressSearches.length; j++) {
-				var searchResult = inProgressSearches[i];
-				if(searchResult.state == "mada") {
-
-					var newSearchResult = searchTrie(bufferStr, searchResult.foundTrieSoFar, searchResult.currentNode, searchResult.offset);
-
-					if(newSearchResult.state == "mada") {
-						tmpArray.push(newSearchResult);
-					} 
-					else if(newSearchResult.state == "success") {
-						successfulSearches.push(newSearchResult);
-					}
-					// throw away failure
-				}
-			}
-
-			inProgressSearches = [];
-
-			// I want to get a substring at each iteration
-			for(var j = 0; j < bufferStr.length; j++) {
-				var bufferSubstr = bufferStr.substring(j);
-
-				var searchResult = searchTrie(bufferSubstr, "", rootTrie, offset);
-
-				if(searchResult.state == "success") {
-					successfulSearches.push(searchResult);
-				}
-				else if(searchResult.state == "mada") {
-					inProgressSearches.push(searchResult);
-				}
-
-				offset = offset + 1;
-			}
-
-		});
-
-		fileStream.on('end', function() {
-		console.log("found the end with " + successfulSearches.length + " succeses");
-			for(var i = 0; i < successfulSearches.length; i++) {
-				console.log(successfulSearches[i].foundTrie + "\t" + successfulSearches[i].offset);
-			}
-		});
+		processFile(filesToProcess[i]);
 	}
 });
+
+var processFile = function(fileName) {
+
+	var fileStream = fs.createReadStream(fileName);
+	var offset = 0;
+
+	var currentWorkSpace = "";
+	var successfulSearches = [];
+
+	fileStream.on('data', function(buffer) {
+
+		print("working on offset# " + offset);
+
+		var remainderBuffer = buffer.toString();
+		var bufferStr = currentWorkSpace + remainderBuffer.substring(0, remainderBuffer.length - maximumTrieSize);
+
+		// I want to get a substring at each iteration
+		for(var j = 0; j < bufferStr.length; j++) {
+			var bufferSubstr = bufferStr.substring(j);
+
+			var searchResult = searchTrie(bufferSubstr, "", rootTrie, offset);
+			
+			if(searchResult.state == "success") {
+				print(searchResult);
+				successfulSearches.push([searchResult.offset, searchResult.foundTrie]);
+			}
+
+			offset = offset + 1;
+		}
+
+		currentWorkSpace = remainderBuffer.substring(remainderBuffer.length - maximumTrieSize);
+	});
+
+	fileStream.on('end', function() {
+		
+		for(var i = 0; i < currentWorkSpace.length; i++) {
+			var subStr = currentWorkSpace.substring(i);
+			var searchResult = searchTrie(subStr, "", rootTrie, offset);
+			if(searchResult.state == "success") {
+				print(searchResult);
+				successfulSearches.push([searchResult.offset, searchResult.foundTrie]);
+			}
+		}
+
+		print("found the end with " + successfulSearches.length + " successful searches");
+
+		console.log(fileName);
+		for(var i = 0; i < successfulSearches.length; i++) {
+			console.log("\t" + successfulSearches[i][0] + "\t" + successfulSearches[i][1]);
+		}
+	});
+}
