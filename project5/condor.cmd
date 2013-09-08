f1567d83165b967de457b5292401afb83a75c53f
Mike Roylance <roylance.michael@gmail.com>
2013-09-08 00:06:24 -0600

    I guess it's good to add in the required argument

--- project5/condor.cmd
+++ project5/condor.cmd
@@ -10,7 +10,7 @@ Universe   = vanilla
 Environment = PATH=/usr/local/bin:/usr/bin:/bin:/condor/bin:/opt/ANT/bin;LC_ALL=en_US.iso88591
  
 Executable  = run.sh
-Arguments   = ./content/ ./content/test.txt
+Arguments   = ./content/ ./content/test.txt ./content/extra-test.txt
 Log         = project5.log
 Output      = output.txt
 Error       = project5.err
